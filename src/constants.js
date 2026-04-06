234bf952e4550f8680b3d6720476b890a6125302
Mayur <mayurbairagi34@gmail.com>
2026-04-06 12:11:16 +0530

    Connect to DB

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+const DB_NAME="TubeHub"
+
+module.exports = { DB_NAME }
---
+++ src/db/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const {DB_NAME} = require('../constants')
+
+const ConnectDB = async () => {
+    try {
+        const ConnectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}${DB_NAME}`)
+        console.log(`Databse Connected Successfully ${ConnectionInstance.connection}`)
+    } catch (error) {
+        console.log(`DATABASE Connection Error FAILED ${error}`)
+        process.exit(1)
+    }
+}
+
+module.exports = ConnectDB
